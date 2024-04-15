a290993ed78534fc10f5a93fbb2c5e4629390560
jiangtao <410518072@qq.com>
2024-04-15 21:59:18 +0800

    修复打包端口问题

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -8,11 +8,16 @@ class App extends Component {
         if (!location.search) {
             return <>需要路径参数，如 ?path=D:\demo.log</>
         }
+        let isDev = process.env.NODE_ENV === "development";
+
 
         let path = location.search.substring(location.search.indexOf('=') + 1);
         path = encodeURIComponent(path)
-        let url = "ws://" + location.hostname + ":8080/api/log-view?path=" + path;
-        return <div style={{width: '100%', height: '100%', minHeight:'100vh'}}>
+
+        let port = isDev ? 8080 : location.port;
+
+        let url = "ws://" + location.hostname + ":" + port + "/api/log-view?path=" + path;
+        return <div style={{width: '100%', height: '100%', minHeight: '100vh'}}>
             <ScrollFollow
                 startFollowing={true}
                 render={({follow, onScroll}) =>
